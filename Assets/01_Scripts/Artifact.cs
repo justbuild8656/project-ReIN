f5a5f9a21cf98d83269b3c430e13d2fbfc0a8075
rain1120 <rain200603@gmail.com>
2025-01-21 00:08:35 +0900

    마도구 제작 시스템(테스트)
    -conflict 테스트

--- Assets/01_Scripts/Artifact.cs
+++ Assets/01_Scripts/Artifact.cs
@@ -7,8 +7,6 @@ public class Artifact
 {
     // TODO: 스탯 가중치 관련 변수,마도구 정보 및 함수추가,마도구 관련 재료 추가
 
-
-    // InProgress : 스탯 가중치 관련 변수 추가중 + 함수이름만
     private List<Ability> abilities;
     private List<Skill> skills;
 }

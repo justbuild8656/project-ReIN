f7fe706dc225c8c5ca1bddf319e900a81b808dbc
FRIDAYFREEBIE <sheepyubin06@gmail.com>
2025-01-20 14:25:31 +0900

    클래스 선택 기능 추가
    작업 내용
    - 클래스 선택 기능을 추가했다.
    - 스크립터블 오브젝트로 클래스를 정의해서 셀렉터에 넣으면 알아서 작동한다.
    - 각 스크립트는 명확한 하나의 책임만 가지도록 세분화 했다.(SRP)
    - 버튼의 각종 이벤트 추가는 클래스 내부를 수정할 필요 없이 이벤트를 등록하는 방식을 사용했다.(OCP)
    - 의존성을 줄이는 구조로 만들어서 기분이좋았다.😀
    수정 할 내용
    - 데이터 테이블이 나오면 테이블을 읽어와서 넣는 방식으로 수정해야 한다.
    - 버튼 그래픽 소스가 나오면 버튼이 아닌 테두리의 색상을 바꾸는 방식으로 수정해야 한다.

---
+++ Assets/01_Scripts/Class/UI/ClassSelector.cs
@@ -0,0 +1,45 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ClassDisplayer))]
+
+// 현재 클래스 목록을 관리
+public class ClassSelector : MonoBehaviour
+{
+  [Header("클래스 목록")]
+  [SerializeField] private List<ClassData> classList;
+
+  [Header("현재 클래스")]
+  [SerializeField] private ClassData currentClass;
+
+  // 클래스 목록 반환
+  public List<ClassData> GetClassList()
+  {
+    if(classList.Count > 0) return classList;
+    else
+    {
+      Debug.LogWarning("classList가 비어있음");
+      return new List<ClassData>();
+    }
+  }
+
+  // 현재 클래스 반환
+  public ClassData GetCurrentClass()
+  {
+    if(currentClass != null) return currentClass;
+    else
+    {
+      Debug.LogWarning("currentClass가 NULL임");
+      return null;
+    }
+  }
+
+  // 현재 클래스 설정
+  public void SetCurrentClass(ClassData newClass)
+  {
+    if(classList.Contains(newClass)) currentClass = newClass;
+    else Debug.LogWarning("newClass가 유효하지 않음");
+  }
+
+  private void Start(){currentClass = null;}
+}

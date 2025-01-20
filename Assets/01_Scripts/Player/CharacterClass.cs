0422281de8d18f89f475d18b98c175008514c1dd
BlueMoon2 <58559207+bluemoontwo@users.noreply.github.com>
2025-01-20 23:02:08 +0900

    Revert "클래스 선택 기능 추가"

--- Assets/01_Scripts/Player/CharacterClass.cs
+++ Assets/01_Scripts/Player/CharacterClass.cs
@@ -4,13 +4,8 @@ using System.Collections.Generic;
 [Serializable]
 public class CharacterClass
 {
-    int id;                 // 클래스 ID
-    int strengthWeight;     // 완력 가중치
-    int intelligenceWeight; // 지력 가중치
-    int staminaWeight;      // 견고 가중치
-    int enduranceWeight;    // 지구 가중치
-    int intuitionWeight;    // 직감 가중치
+    // TODO: 스탯 가중치 관련 변수 및 함수추가
     
-    private List<Ability> abilities; // 어빌리티 리스트
-    private List<Skill> skills;      // 스킬 리스트
+    private List<Ability> abilities;
+    private List<Skill> skills;
 }

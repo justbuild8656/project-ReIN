c71f14e0de0fac7eb17fd077a7ebd7dc7f760fa9
rain1120 <rain200603@gmail.com>
2025-01-21 19:39:40 +0900

    깃허브 컨플릭트 테스트

--- Assets/01_Scripts/Artifact.cs
+++ Assets/01_Scripts/Artifact.cs
@@ -1,11 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 
-
 [Serializable]
 public class Artifact
 {
-    // TODO: 스탯 가중치 관련 변수,마도구 정보 및 함수추가,마도구 관련 재료 추가
+    // TODO: 스탯 가중치 관련 변수 및 함수추가
 
     private List<Ability> abilities;
     private List<Skill> skills;
